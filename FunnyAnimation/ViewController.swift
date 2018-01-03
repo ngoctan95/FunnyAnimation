5cbf11ee1334f419146eb111be09b4383aa366c1
ngoctan95 <nguyenngoctan44@gmail.com>
2018-01-03 23:14:25 +0700

    Using animation

--- FunnyAnimation/ViewController.swift
+++ FunnyAnimation/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblText: UILabel!
+    @IBOutlet weak var lblImg: UIImageView!
+    
+    /*
+     Compare between viewDidLoad and ViewDiaAppear or ViewWillAppear:
+     ViewDidAppear: one time for displaying, suitable for static data.
+     ViewWillAppear or ViewDidAppear: multiple time, suitable for dynamic data.
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*First, img and label have their alpha (opacity = 0 )
+         */
+        
+        lblText!.alpha = 0
+        lblImg!.alpha = 0
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2, animations: {
+            self.lblImg!.alpha = 0.9
+        }) { (animated) in
+            UIView.animate(withDuration: 3, animations: {
+                self.lblText.alpha = 1
+                self.lblText.center = CGPoint(x: self.lblImg.center.x, y: 50)
+                self.lblImg.center = CGPoint (x: self.lblImg.center.x, y: 400)
+            }, completion: { (finished) in
+                self.lblImg.alpha = 1
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
